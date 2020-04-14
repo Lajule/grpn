520aa0fb0c78b22a53a15bc83039517327b62400
Julien Rouzieres <julien.rouzieres@mac.com>
2020-04-14 17:54:29 +0200

    Add tools and docs

---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cwd=$PWD
+
+for goos in darwin linux windows; do
+	for goarch in 386 amd64; do
+		(cd ${0%/*} && GOOS=$goos GOARCH=$goarch go build -o $cwd/grpn_${goos}_$goarch)
+	done
+done
